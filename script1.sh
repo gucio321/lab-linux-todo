7bf6605105dd9a392ab221796d1d6935214499bf
gucio321 <gucio321@protonmail.com>
2025-11-14 15:24:28 +0100

    implement some logic for add operation

--- script1.sh
+++ script1.sh
@@ -11,6 +11,17 @@ Invalid command - usage:
 EOF
 }
 
+function validateArgs() {
+        min=$1
+        max=$2
+        actual=$3
+        if [[ $actual -lt $min || $actual -gt $max ]]; then
+                echo "Invalid number of argumetns! At least $min at most $max expected (got $actual)!"
+                usage
+                exit 1
+        fi
+}
+
 if [[ $# == 0 ]]; then
         usage
         exit 1
@@ -20,7 +31,42 @@ cmd=$1
 shift # for convinience ;-)
 case $cmd in
         add)
-                echo "dupa"
+                validateArgs 2 3 $#
+                evDate=$(date -d "$1" +%s 2> /dev/null)
+                if [[ $? != 0 ]]; then
+                        echo "Inalid date format!"
+                        usage
+                        exit 2
+                fi
+                # validate date
+                # %s is a second since the epoch (comfortable for integer operations)
+                today=$(date +%s)
+                evDescription=$2
+                if [[ $evDate -lt $today ]]; then
+                        echo "Date cannot be in the past!"
+                        usage
+                        exit 3
+                fi
+                # also date cannot be too far in the future (say one year)
+                # well this might intoduce some small error due to +1 year considering current time, but its acceptable for me ;-)
+                oneYearFromToday=$(date -d "+1 year" +%s)
+                if [[ $evDate -gt $oneYearFromToday ]]; then
+                        echo "Date cannot be more than one year in the future!"
+                        usage
+                        exit 3
+                fi
+
+                priority=$3
+                if [[ $priority == "" ]]; then
+                        priority="0"
+                fi
+
+                cat << EOF
+Adding new event:
+- When?: $(date -d "@$evDate" +"%Y-%m-%d")
+- What?: $evDescription
+- Priority: $priority
+EOF
                 ;;
         --help)
                 usage
