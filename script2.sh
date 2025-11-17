736c73e9db99ce775d9408c4948737cefc99acfd
gucio321 <gucio321@protonmail.com>
2025-11-17 15:24:49 +0100

    add sed4

--- script2.sh
+++ script2.sh
@@ -2,4 +2,5 @@
 sed -n -f sed1.sed read_komentarze.c > sed1.out
 sed -f sed2.sed read_komentarze.c > sed2.out
 sed -f sed3.sed read_komentarze.c > sed3.out
-#sed --debug -f sed2.sed test.txt # > sed2.out
+sed -f sed4.sed read_komentarze.c > sed4.out
+#sed -f sed4.sed test.txt # > sed2.out
