1faf345cc6b771cb551b19665a03d9fc319f329c
gucio321 <gucio321@protonmail.com>
2025-11-18 14:45:12 +0100

    add 6th scirpt

--- script2.sh
+++ script2.sh
@@ -4,4 +4,4 @@ sed -f sed2.sed read_komentarze.c > sed2.out
 sed -f sed3.sed read_komentarze.c > sed3.out
 sed -f sed4.sed read_komentarze.c > sed4.out
 sed -f sed5.sed read_komentarze.c > sed5.out
-#sed -f sed4.sed test.txt # > sed2.out
+sed -f sed6.sed read_komentarze.c > sed6.out
